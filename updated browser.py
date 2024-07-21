90efe4d3b8dcabc52191accaaa8ceef374862d4e
Rewad hyuujh <131447858+fjddydtsyrawe636i5whdew@users.noreply.github.com>
2024-07-21 09:15:09 +0530

    Create updated browser.py

---
+++ updated browser.py
@@ -0,0 +1,211 @@
+import contextlib
+import logging
+import random
+import uuid
+from pathlib import Path
+from typing import Any
+
+import ipapi
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+from src.userAgentGenerator import GenerateUserAgent
+from src.utils import Utils
+
+
+DEFAULT_SLEEP = 80
+class Browser:
+    """WebDriver wrapper class."""
+
+    def __init__(self, mobile: bool, account, args: Any) -> None:
+        self.mobile = mobile
+        self.browserType = "mobile" if mobile else "desktop"
+        self.headless = not args.visible
+        self.username = account["username"]
+        self.password = account["password"]
+        try:
+            self.sleep = account["sleep"]
+        except Exception:
+            self.sleep = DEFAULT_SLEEP
+        logging.info(
+            f'[BROWSER] { self.sleep } seconds between searches '
+        )
+        self.localeLang, self.localeGeo = self.getCCodeLang(args.lang, args.geo)
+        self.proxy = None
+        if args.proxy:
+            self.proxy = args.proxy
+        elif account.get("proxy"):
+            self.proxy = account["proxy"]
+        self.userDataDir = self.setupProfiles()
+        self.browserConfig = Utils.getBrowserConfig(self.userDataDir)
+        (
+            self.userAgent,
+            self.userAgentMetadata,
+            newBrowserConfig,
+        ) = GenerateUserAgent().userAgent(self.browserConfig, mobile)
+        if newBrowserConfig:
+            self.browserConfig = newBrowserConfig
+            Utils.saveBrowserConfig(self.userDataDir, self.browserConfig)
+        self.webdriver = self.browserSetup()
+        self.utils = Utils(self.webdriver)
+
+    def __enter__(self) -> "Browser":
+        return self
+
+    def __exit__(self, *args: Any) -> None:
+        self.closeBrowser()
+
+    def closeBrowser(self) -> None:
+        """Perform actions to close the browser cleanly."""
+        # close web browser
+        with contextlib.suppress(Exception):
+            self.webdriver.close()
+            self.webdriver.quit()
+
+    def browserSetup(
+        self,
+    ) -> WebDriver:
+        options = webdriver.ChromeOptions()
+        options.headless = self.headless
+        options.add_argument(f"--lang={self.localeLang}")
+        options.add_argument("--log-level=3")
+        options.add_argument("--blink-settings=imagesEnabled=false")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--ignore-certificate-errors-spki-list")
+        options.add_argument("--ignore-ssl-errors")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--dns-prefetch-disable")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-default-apps")
+        options.add_argument("--disable-features=Translate")
+        options.add_argument('--disable-features=PrivacySandboxSettings4')
+
+        if self.proxy:
+            options.add_argument(f'--proxy-server={self.proxy}')
+
+        # Obtain webdriver chrome driver version
+        version = self.getChromeVersion()
+        major = int(version.split(".")[0])
+
+        driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),
+            options=options,
+        )
+
+        seleniumLogger = logging.getLogger("selenium")
+        seleniumLogger.setLevel(logging.ERROR)
+
+        if self.browserConfig.get("sizes"):
+            deviceHeight = self.browserConfig["sizes"]["height"]
+            deviceWidth = self.browserConfig["sizes"]["width"]
+        else:
+            if self.mobile:
+                deviceHeight = random.randint(568, 1024)
+                deviceWidth = random.randint(320, min(576, int(deviceHeight * 0.7)))
+            else:
+                deviceWidth = random.randint(1024, 2560)
+                deviceHeight = random.randint(768, min(1440, int(deviceWidth * 0.8)))
+            self.browserConfig["sizes"] = {
+                "height": deviceHeight,
+                "width": deviceWidth,
+            }
+            Utils.saveBrowserConfig(self.userDataDir, self.browserConfig)
+
+        if self.mobile:
+            screenHeight = deviceHeight + 146
+            screenWidth = deviceWidth
+        else:
+            screenWidth = deviceWidth + 55
+            screenHeight = deviceHeight + 151
+
+        logging.info(f"Screen size: {screenWidth}x{screenHeight}")
+        logging.info(f"Device size: {deviceWidth}x{deviceHeight}")
+
+        if self.mobile:
+            driver.execute_cdp_cmd(
+                "Emulation.setTouchEmulationEnabled",
+                {
+                    "enabled": True,
+                },
+            )
+
+        driver.execute_cdp_cmd(
+            "Emulation.setDeviceMetricsOverride",
+            {
+                "width": deviceWidth,
+                "height": deviceHeight,
+                "deviceScaleFactor": 0,
+                "mobile": self.mobile,
+                "screenWidth": screenWidth,
+                "screenHeight": screenHeight,
+                "positionX": 0,
+                "positionY": 0,
+                "viewport": {
+                    "x": 0,
+                    "y": 0,
+                    "width": deviceWidth,
+                    "height": deviceHeight,
+                    "scale": 1,
+                },
+            },
+        )
+
+        driver.execute_cdp_cmd(
+            "Emulation.setUserAgentOverride",
+            {
+                "userAgent": self.userAgent,
+                "platform": self.userAgentMetadata["platform"],
+                "userAgentMetadata": self.userAgentMetadata,
+            },
+        )
+        # Set timeout to something bigger that account sleep
+        timeout = self.sleep + 5
+        logging.info(
+            f'[BROWSER] Default timeout:{timeout}'
+        )
+        driver.set_page_load_timeout(timeout)
+        return driver
+
+    def setupProfiles(self) -> Path:
+        """
+        Sets up the sessions profile for the chrome browser.
+        Uses the username to create a unique profile for the session.
+
+        Returns:
+            Path
+        """
+        currentPath = Path(__file__)
+        parent = currentPath.parent.parent
+        sessionsDir = parent / "sessions"
+
+        sessionUuid = uuid.uuid5(uuid.NAMESPACE_DNS, self.username)
+        sessionsDir = sessionsDir / str(sessionUuid) / self.browserType
+        sessionsDir.mkdir(parents=True, exist_ok=True)
+        return sessionsDir
+
+    def getCCodeLang(self, lang: str, geo: str) -> tuple:
+        if lang is None or geo is None:
+            try:
+                nfo = ipapi.location()
+                if isinstance(nfo, dict):
+                    if lang is None:
+                        lang = nfo["languages"].split(",")[0].split("-")[0]
+                    if geo is None:
+                        geo = nfo["country"]
+            except Exception:  # pylint: disable=broad-except
+                return ("en", "US")
+        return (lang, geo)
+
+    def getChromeVersion(self) -> str:
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        driver = WebDriver(options=chrome_options)
+        version = driver.capabilities["browserVersion"]
+
+        driver.quit()
+
+        return version
